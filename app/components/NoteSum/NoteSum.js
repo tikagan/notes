eb978ea73a412ae6caf055b60a61966661065f1e
kritika <kritika@PWNbook-Pro.local>
2018-05-15 18:11:12 -0400

    setup initial components

---
+++ app/components/NoteSum/NoteSum.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class NoteSum extends Component {
+  // static propTypes = {
+  //   increment: PropTypes.func.isRequired,
+  //   incrementIfOdd: PropTypes.func.isRequired,
+  //   incrementAsync: PropTypes.func.isRequired,
+  //   decrement: PropTypes.func.isRequired,
+  //   counter: PropTypes.number.isRequired
+  };
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default NoteSum;
